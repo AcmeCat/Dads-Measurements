49580713b68be4553dd5144604b3f89a9a13e5a3
AcmeCat <31060311+AcmeCat@users.noreply.github.com>
2024-03-31 14:50:30 +0800

    Initial Commit

---
+++ Dads Measurements/Dads_MeasurementsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dads_MeasurementsApp.swift
+//  Dads Measurements
+//
+//  Created by Rory Allen on 31/3/2024.
+//
+
+import SwiftUI
+
+@main
+struct Dads_MeasurementsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
